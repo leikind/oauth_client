a299995e09c1cc68bc8dded53c42cdedbbdd0ed0
Yuri Leikind <yuri.leikind@gmail.com>
2018-10-01 16:47:51 +0200

    disable HTTP basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  http_basic_authenticate_with(
-    name: Rails.application.secrets.http_basic_authentication_name,
-    password: Rails.application.secrets.http_basic_authentication_password
-  )
+  # http_basic_authenticate_with(
+  #   name: Rails.application.secrets.http_basic_authentication_name,
+  #   password: Rails.application.secrets.http_basic_authentication_password
+  # )
 
   helper_method :current_user
   helper_method :user_signed_in?
